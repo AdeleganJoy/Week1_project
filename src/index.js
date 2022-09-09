2fb616ca76ea8b89a26f9c8a6b4bff32ee1a8c5d
AdeleganJoy <96977916+AdeleganJoy@users.noreply.github.com>
2022-09-09 21:58:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ function display_message(){
     document.getElementById("demo").innerHTML = "My notebook";
 }
 
+
+function display_list(){
+    document.getElementById("list").innerHTML = '<li>Carrot</li>';
+}
+
 window.onload = function() {
-    var button = document.getElementById("my-button");
-    button.onclick = display_message;
+    var button_1 = document.getElementById("add-data");
+    var button_2 = document.getElementById("my-button");
+    button_1.onclick = display_list;
+    button_2.onclick = display_message;
 }
